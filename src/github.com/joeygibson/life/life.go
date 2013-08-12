02a400b81a7bff8b779bab72d7dded1eaecb762c
Joey Gibson <jgibson@lancope.com>
2013-08-12 12:51:33 -0400

    added license

--- src/github.com/joeygibson/life/life.go
+++ src/github.com/joeygibson/life/life.go
@@ -1,10 +1,33 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) [year] [fullname]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 package main
 
 import (
 	"flag"
 	"fmt"
-	"github.com/nsf/termbox-go"
 	"github.com/joeygibson/life/entities"
+	"github.com/nsf/termbox-go"
 	"os"
 )
 
@@ -29,7 +52,7 @@ func main() {
 	}
 
 	board := entities.NewBoard(*size, *sleepTime)
-	
+
 	if *hackerSeed {
 		board.HackerEmblemSeed()
 	} else {
@@ -52,6 +75,6 @@ func main() {
 			entities.EventChan <- termbox.PollEvent()
 		}
 	}()
-	
+
 	board.Play(*iterations)
 }

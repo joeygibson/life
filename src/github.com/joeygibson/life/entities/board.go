25a5d0f3048d1256436be8aaba324179c95f4310
Joey Gibson <joey@joeygibson.com>
2013-08-16 22:34:55 -0400

    almost working with non-identical width and height

--- src/github.com/joeygibson/life/entities/board.go
+++ src/github.com/joeygibson/life/entities/board.go
@@ -24,56 +24,28 @@ SOFTWARE.
 package entities
 
 import (
-	"github.com/nsf/termbox-go"
 	"math/rand"
-	"os"
-	"os/exec"
 	"time"
 )
 
 type Board struct {
-	size int
-	sleepTime int
-	cells [][]Cell
+	Rows int
+	Columns int
+	SleepTime int
+	Cells [][]Cell
 }
 
-var EventChan chan termbox.Event
-
-func (board Board) DisplayBoard() {
-	termbox.SetCell(0, 0, '\u250c', termbox.ColorDefault, termbox.ColorDefault)
-	for i := 0; i < board.size; i++ {
-		termbox.SetCell(i + 1, 0, '\u2500', termbox.ColorDefault, termbox.ColorDefault)
-	}
-	termbox.SetCell(board.size + 1, 0, '\u2510', termbox.ColorDefault, termbox.ColorDefault)
-	
-	for i := 0; i < board.size; i++ {
-		termbox.SetCell(0, i + 1, '\u2502', termbox.ColorDefault, termbox.ColorDefault)
-		for j := 0; j < board.size; j++ {
-			termbox.SetCell(i + 1, j + 1, board.cells[i][j].Rune(), termbox.ColorDefault, termbox.ColorDefault)
-		}
-		termbox.SetCell(board.size + 1, i + 1, '\u2502', termbox.ColorDefault, termbox.ColorDefault)
-	}
-
-	termbox.SetCell(0, board.size + 1, '\u2514', termbox.ColorDefault, termbox.ColorDefault)
-	for i := 0; i < board.size; i++ {
-		termbox.SetCell(i + 1, board.size + 1, '\u2500', termbox.ColorDefault, termbox.ColorDefault)
-	}
-	termbox.SetCell(board.size + 1, board.size + 1, '\u2518', termbox.ColorDefault, termbox.ColorDefault)
-	
-	termbox.Flush()
-}
-
-func (board Board) GetNeighbors(x, y int) []Cell {
+func (board Board) GetNeighbors(r, c int) []Cell {
 	var cells []Cell
 
 	for i := -1; i < 2; i++ {
 		for j := -1; j < 2; j++ {
-			newX, newY := x + i, y + j
+			newR, newC := r + i, c + j
 
-			if newX >= 0 && newY >= 0 &&
-				newX < board.size && newY < board.size &&
-				!(newX == x && newY == y) {
-				cells = append(cells, board.cells[newX][newY])
+			if newR >= 0 && newC >= 0 &&
+				newR < board.Rows && newC < board.Columns &&
+				!(newR == r && newC == c) {
+				cells = append(cells, board.Cells[newR][newC])
 			}
 		}
 	}
@@ -81,79 +53,42 @@ func (board Board) GetNeighbors(x, y int) []Cell {
 	return cells
 }
 
-func (board *Board) Step() {
-	var newCells = createCells(board.size)
+func (board Board) Step() Board {
+	var newCells = createCells(board.Rows, board.Columns)
 
-	for i := 0; i < board.size; i++ {
-		for j := 0; j < board.size; j++ {
-			newCells[i][j] = board.cells[i][j].Step(board.GetNeighbors(i, j))
+	for i := 0; i < board.Rows; i++ {
+		for j := 0; j < board.Columns; j++ {
+			newCells[i][j] = board.Cells[i][j].Step(board.GetNeighbors(i, j))
 		}
 	}
 
-	board.cells = newCells
-
-	board.DisplayBoard()
+	board.Cells = newCells
+	
+	return board
 }
 
-func (board Board) Play(iterations int) {
-	if iterations < 0 {
-		for {
-			board.Step()
-			
-			select {
-				case ev := <- EventChan:
-					if ev.Type == termbox.EventKey && ev.Key == termbox.KeyEsc {
-						return
-					}
-				default:
-					// nothing
-			}
-			
-			board.Sleep()
-		}
-	} else {
-		for i := 0; i < iterations; i++ {
-			board.Step()
-
-			select {
-				case ev := <- EventChan:
-					if ev.Type == termbox.EventKey && ev.Key == termbox.KeyEsc {
-						return
-					}
-				default:
-					// nothing
-			}
-			
-			board.Sleep()
-		}
-	}
-}
-
-func (board Board) Sleep() {
-	time.Sleep(time.Duration(board.sleepTime) * time.Millisecond)
-}
 
 func (board *Board) HackerEmblemSeed() {
-	board.cells[0][2].SetAlive(true)
-	board.cells[1][0].SetAlive(true)
-	board.cells[1][2].SetAlive(true)
-	board.cells[2][1].SetAlive(true)
-	board.cells[2][2].SetAlive(true)
+	board.Cells[0][2].SetAlive(true)
+	board.Cells[1][0].SetAlive(true)
+	board.Cells[1][2].SetAlive(true)
+	board.Cells[2][1].SetAlive(true)
+	board.Cells[2][2].SetAlive(true)
 }
 
 func (board *Board) TwoIslandPseudoStillLifeSeed() {
-	board.cells[1][0].SetAlive(true)
-	board.cells[2][0].SetAlive(true)
-	board.cells[4][0].SetAlive(true)
-	board.cells[5][0].SetAlive(true)
-    board.cells[2][1].SetAlive(true)
-    board.cells[4][1].SetAlive(true)
-    board.cells[2][2].SetAlive(true)
-    board.cells[4][2].SetAlive(true)
-    board.cells[1][3].SetAlive(true)
-    board.cells[2][3].SetAlive(true)
-    board.cells[4][3].SetAlive(true)
-    board.cells[5][3].SetAlive(true)
+	board.Cells[1][0].SetAlive(true)
+	board.Cells[2][0].SetAlive(true)
+	board.Cells[4][0].SetAlive(true)
+	board.Cells[5][0].SetAlive(true)
+    board.Cells[2][1].SetAlive(true)
+    board.Cells[4][1].SetAlive(true)
+    board.Cells[2][2].SetAlive(true)
+    board.Cells[4][2].SetAlive(true)
+    board.Cells[1][3].SetAlive(true)
+    board.Cells[2][3].SetAlive(true)
+    board.Cells[4][3].SetAlive(true)
+    board.Cells[5][3].SetAlive(true)
 }
 
 // Pseudo-random seeding of the board
@@ -163,40 +98,34 @@ func (board *Board) Seed() {
 	times := 0
 	
 	for times == 0 {
-		times = r.Intn(board.size * board.size)
+		times = r.Intn(board.Rows * board.Columns)
 	}
 		
 	for t := 0; t < times; t++ {
-		i, j := rand.Intn(board.size), rand.Intn(board.size)
+		i, j := rand.Intn(board.Rows), rand.Intn(board.Columns)
 		
-		board.cells[i][j].SetAlive(true)
+		board.Cells[i][j].SetAlive(true)
 	}
 }
 
-func createCells(size int) [][]Cell {
-	cells := make([][]Cell, size)
+func createCells(Rows, Columns int) [][]Cell {
+	cells := make([][]Cell, Rows)
 	
-	for i := 0; i < size; i++ {
-		row := make([]Cell, size)
+	for i := 0; i < Rows; i++ {
+		row := make([]Cell, Columns)
 		cells[i] = row
 	}
 
 	return cells
 }
 
-func NewBoard(size int, sleepTime int) Board {
-	EventChan = make(chan termbox.Event)
+func NewBoard(Rows, Columns int, SleepTime int) Board {
 	var board Board
 
-	board.size = size
-	board.sleepTime = sleepTime
-	board.cells = createCells(size)
+	board.Rows = Rows
+	board.Columns = Columns
+	board.SleepTime = SleepTime
+	board.Cells = createCells(Rows, Columns)
 
 	return board
-}
-
-func ClearScreen() {
-	c := exec.Command("clear")
-	c.Stdout = os.Stdout
-	c.Run()
 }
--- src/github.com/joeygibson/life/life.go
+++ src/github.com/joeygibson/life/life.go
@@ -29,13 +29,17 @@ import (
 	"github.com/joeygibson/life/entities"
 	"github.com/nsf/termbox-go"
 	"os"
+	"time"
 )
 
-var size = flag.Int("s", 20, "Row/Column size")
+var columns = flag.Int("c", -1, "number of columns of board")
+var rows = flag.Int("r", -1, "number of rows of board")
 var iterations = flag.Int("i", -1, "# of iterations")
 var help = flag.Bool("h", false, "display help")
 var hackerSeed = flag.Bool("H", false, "seed with the hacker emblem")
 var sleepTime = flag.Int("w", 500, "milliseconds to sleep between iterations")
+var stopping = false
+var EventChan chan termbox.Event
 
 func Usage() {
 	fmt.Fprintf(os.Stderr, "Usage: life [options]\n")
@@ -51,7 +55,7 @@ func main() {
 		Usage()
 	}
 
-	board := entities.NewBoard(*size, *sleepTime)
+	board := entities.NewBoard(*rows, *columns, *sleepTime)
 
 	if *hackerSeed {
 		board.HackerEmblemSeed()
@@ -66,15 +70,84 @@ func main() {
 
 	defer termbox.Close()
 
+	EventChan = make(chan termbox.Event)
+
 	termbox.SetInputMode(termbox.InputEsc)
 	termbox.HideCursor()
 	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
 
 	go func() {
 		for {
-			entities.EventChan <- termbox.PollEvent()
+			select {
+			case ev := <-EventChan:
+				if ev.Type == termbox.EventKey && ev.Key == termbox.KeyEsc {
+					stopping = true
+				}
+			default:
+				// nothing
+			}
 		}
 	}()
 
-	board.Play(*iterations)
+	go func() {
+		for {
+			EventChan <- termbox.PollEvent()
+		}
+	}()
+
+	Play(board, *iterations)
+}
+
+func Play(board entities.Board, iterations int) {
+	if iterations < 0 {
+		for {
+			if stopping {
+				return
+			}
+			
+			board = board.Step()
+			displayBoard(board)
+			
+			Sleep()
+		}
+	} else {
+		for i := 0; i < iterations; i++ {
+			if stopping {
+				return
+			}
+			
+			board = board.Step()
+			displayBoard(board)
+			
+			Sleep()
+		}
+	}
+}
+
+func displayBoard(board entities.Board) {
+	termbox.SetCell(0, 0, '\u250c', termbox.ColorDefault, termbox.ColorDefault)
+	for i := 0; i < board.Columns; i++ {
+		termbox.SetCell(i + 1, 0, '\u2500', termbox.ColorDefault, termbox.ColorDefault)
+	}
+	termbox.SetCell(board.Columns + 1, 0, '\u2510', termbox.ColorDefault, termbox.ColorDefault)
+	
+	for i := 0; i < board.Rows; i++ {
+		termbox.SetCell(0, i + 1, '\u2502', termbox.ColorDefault, termbox.ColorDefault)
+		for j := 0; j < board.Columns; j++ {
+			termbox.SetCell(i + 1, j + 1, board.Cells[i][j].Rune(), termbox.ColorDefault, termbox.ColorDefault)
+		}
+		termbox.SetCell(board.Columns + 1, i + 1, '\u2502', termbox.ColorDefault, termbox.ColorDefault)
+	}
+
+	termbox.SetCell(0, board.Rows + 1, '\u2514', termbox.ColorDefault, termbox.ColorDefault)
+	for i := 0; i < board.Columns; i++ {
+		termbox.SetCell(i + 1, board.Columns + 1, '\u2500', termbox.ColorDefault, termbox.ColorDefault)
+	}
+	termbox.SetCell(board.Rows + 1, board.Columns + 1, '\u2518', termbox.ColorDefault, termbox.ColorDefault)
+	
+	termbox.Flush()
+}
+
+func Sleep() {
+	time.Sleep(time.Duration(*sleepTime) * time.Millisecond)
 }
